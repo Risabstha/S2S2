16f5a66013cc1a22bfff4a45584e924ef08d1e79
Risab Shrestha <risabshrestha50@gmail.com>
2026-03-19 00:05:55 +0545

    final

--- client/src/pages/client/Collabration.tsx
+++ client/src/pages/client/Collabration.tsx
@@ -1,7 +1,7 @@
 // import ardc from "../../assets/client/partner/Ardc.jpeg"
 import unoosa from "../../assets/client/partner/UNOOSA.webp";
-import antarikc from "../../assets/client/partner/antarikchya.webp";
-import s2s2 from "../../assets/client/s2s2logo.webp";
+// import antarikc from "../../assets/client/partner/antarikchya.webp";
+// import s2s2 from "../../assets/client/s2s2logo.webp";
 
 const Collabration = () => {
   return (
@@ -12,8 +12,8 @@ const Collabration = () => {
       </p>
       <div className="flex justify-center m-auto items-center flex-wrap gap-x-6 gap-y-4 max-w-full md:max-w-none md:flex-nowrap md:gap-x-10">
         {/* <img src={ardc} alt="ARDC Logo" width={110}  /> */}
-        <img src={antarikc} alt="Antarikc Logo" className="basis-[45%] max-w-25 md:basis-auto lg:w-27.5 md:w-25 w-20" />
-        <img src={s2s2} alt="S2S2 Logo" className="basis-[45%] max-w-25 md:basis-auto lg:w-27.5 md:w-25 w-20" />
+        {/* <img src={antarikc} alt="Antarikc Logo" className="basis-[45%] max-w-25 md:basis-auto lg:w-27.5 md:w-25 w-20" /> */}
+        {/* <img src={s2s2} alt="S2S2 Logo" className="basis-[45%] max-w-25 md:basis-auto lg:w-27.5 md:w-25 w-20" /> */}
         <img src={unoosa} alt="UNOOSA Logo" className="basis-[45%] max-w-30 md:basis-auto lg:w-32.5 md:w-30 w-25" />
       </div>
 
